7ac4d037d90ec337b1485c6bc8c318711e599597
Toby-donghunseo <donghun.seo@sk.com>
2021-06-23 01:39:36 +0900

    Ex3-3

---
+++ example03-01.py
@@ -0,0 +1,169 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F 
+from torchvision import transforms, datasets
+
+if torch.cuda.is_available():
+    DEVICE = torch.device('cuda')
+else:
+    DEVICE = torch.device('cpu')
+
+print(f"Using Pytorch vesion: {torch.__version__}, Device: {DEVICE}")
+
+BATCH_SIZE = 32
+EPOCHS = 10
+
+train_dataset = datasets.MNIST(root = f"data/MNIST",
+                                train = True,
+                                download = True,
+                                transform = transforms.ToTensor())
+
+test_dataset = datasets.MNIST(root = f"data/MNIST",
+                                train = False,
+                                transform = transforms.ToTensor())
+
+train_loader = torch.utils.data.DataLoader(dataset = train_dataset,
+                                            batch_size = BATCH_SIZE,
+                                            shuffle = True)
+
+test_loader = torch.utils.data.DataLoader(dataset = test_dataset, 
+                                            batch_size = BATCH_SIZE,
+                                            shuffle = False)
+
+for (X_train, y_train) in train_loader:
+    print(f"X_train: {X_train.size()}, type: {X_train.type()}")
+    print(f"y_train: {y_train.size()}, type: {y_train.type()}")
+    break
+
+pltsize = 1
+plt.figure(figsize=(10*pltsize, pltsize))
+for i in range(10):
+    plt.subplot(1, 10, i+1)
+    plt.axis(f"off")
+    plt.imshow(X_train[i,:,:,:].numpy().reshape(28,28), cmap="gray_r")
+    plt.title(f"Class: {y_train[i].item()}")
+plt.show()
+
+
+########################################
+## Ex3-1. Dropout
+'''
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(28*28, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 10)
+        self.dropout_prob = 0.5
+
+    def forward(self, x):
+        x = x.view(-1, 28*28)
+        x = self.fc1(x)
+        x = F.sigmoid(x)
+        x = F.dropout(x, training=self.training, p=self.dropout_prob)
+        x = self.fc2(x) 
+        x = F.sigmoid(x)
+        x = F.dropout(x, training=self.training, p=self.dropout_prob)
+        x = self.fc3(x)
+        x = F.log_softmax(x, dim=1)
+        return x 
+'''
+########################################
+
+########################################
+## Ex3-2. Dropout+ReLU
+'''
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(28*28, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 10)
+        self.dropout_prob = 0.5
+
+    def forward(self, x):
+        x = x.view(-1, 28*28)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training, p=self.dropout_prob)
+        x = self.fc2(x) 
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training, p=self.dropout_prob)
+        x = self.fc3(x)
+        x = F.log_softmax(x, dim=1)
+        return x 
+'''
+########################################
+
+########################################
+## Ex3-3. Dropout + ReLU + Batch Normalization 
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(28*28, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 10)
+        self.dropout_prob = 0.5
+        self.batch_norm1 = nn.BatchNorm1d(512)
+        self.batch_norm2 = nn.BatchNorm1d(256)
+
+    def forward(self, x):
+        x = x.view(-1, 28*28)
+        x = self.fc1(x)
+        x = self.batch_norm1(x)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training, p=self.dropout_prob)
+        x = self.fc2(x) 
+        x = self.batch_norm2(x)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training, p=self.dropout_prob)
+        x = self.fc3(x)
+        x = F.log_softmax(x, dim=1)
+        return x 
+
+model = Net().to(DEVICE)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+criterion = nn.CrossEntropyLoss()
+
+print(model)
+
+def train(model, train_loader, optimizer, log_interval):
+    model.train()
+    for batch_idx, (image, label) in enumerate(train_loader):
+        image = image.to(DEVICE)
+        label = label.to(DEVICE)
+        optimizer.zero_grad()
+        output = model(image)
+        loss = criterion(output, label)
+        loss.backward()
+        optimizer.step()
+
+        if batch_idx % log_interval == 0:
+            print(f"Train Epoch: [{batch_idx*len(image)}/{len(train_loader.dataset)}()%)]\tTrain loss: {loss.item()}")
+
+def evaluate(model, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for image, label in test_loader:
+            image = image.to(DEVICE)
+            label = label.to(DEVICE)
+            output = model(image)
+            test_loss += criterion(output, label).item()
+            prediction = output.max(1, keepdim = True)[1]
+            correct += prediction.eq(label.view_as(prediction)).sum().item()
+        
+    test_loss /= len(test_loader.dataset)
+    test_accuracy = 100. * correct / len(test_loader.dataset)
+    return test_loss, test_accuracy 
+
+for Epoch in range(1, EPOCHS+1):
+    train(model, train_loader, optimizer, log_interval = 200)
+    test_loss, test_accuracy = evaluate(model, test_loader)
+    print(f"\n[EPOCH: {Epoch}], \tTest loss: {test_loss}\tTest accuracy: {test_accuracy}")
+    
